75d75290ab34c01af1b3849d2c6bfaba4e40af02
INDRA\bfelix <bfelix@indracompany.com>
2022-05-25 09:51:43 -0300

    :tada: Criação do projeto

---
+++ src/test/kotlin/me/brunofelix/mercadolivro/MercadoLivroApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.brunofelix.mercadolivro
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MercadoLivroApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
