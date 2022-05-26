64bca8935fb3947bbb0dc033c03f410ba377183b
INDRA\bfelix <bfelix@indracompany.com>
2022-05-25 21:17:10 -0300

    :sparkles: 32. Recebendo um Path Param

--- src/main/kotlin/me/brunofelix/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/me/brunofelix/mercadolivro/controller/CustomerController.kt
@@ -4,6 +4,7 @@ import me.brunofelix.mercadolivro.controller.request.CustomerRequest
 import me.brunofelix.mercadolivro.model.CustomerModel
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -14,14 +15,27 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("customers")
 class CustomerController {
 
+    val customersList = mutableListOf<CustomerModel>()
+
     @GetMapping
-    fun getCustomer(): CustomerModel {
-        return CustomerModel("1", "Bruno", "brunofelix.dev@gmail.com")
+    fun getAll(): List<CustomerModel> {
+        return customersList
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     fun create(@RequestBody customer: CustomerRequest) {
-        println(customer)
+        val id = if (customersList.isEmpty()) {
+            1
+        } else {
+            customersList.last().id.toInt() + 1
+        }.toString()
+
+        customersList.add(CustomerModel(id, customer.name, customer.email))
+    }
+
+    @GetMapping("/{id}")
+    fun getCustomer(@PathVariable id: String): CustomerModel {
+        return customersList.filter { it.id == id }.first()
     }
 }
